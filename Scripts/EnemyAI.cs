421042d13cac026b4ff0b8b4c0e6144acf45b8fe
ClamDaButther <37106669+ClamDaButther@users.noreply.github.com>
2018-05-22 10:10:03 +0200

    Add files via upload

---
+++ Scripts/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    public Transform[] patrolPoints;
+    public float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+
+	void Start () {
+        currentPatrolIndex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
+
+    }
+	
+	
+	void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (Vector3.Distance (transform.position,currentPatrolPoint.position) < .1f)
+        {
+            if (currentPatrolIndex + 1 < patrolPoints.Length)
+            {
+                currentPatrolIndex++;
+            }
+            else
+            {
+                currentPatrolIndex = 0;
+            }
+            currentPatrolPoint = patrolPoints[currentPatrolIndex];
+        }
+        //dit is dat hij naar zijn patrolpoint kijkt <3
+        Vector3 patrolPointDir = currentPatrolPoint.position - transform.position;
+        float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg - 0f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 50f);
+    }
+
+   
+
+}
