c057e41a95b7190b48fa52670e6dd20ce90e6c3e
ClamDaButther <37106669+ClamDaButther@users.noreply.github.com>
2018-05-24 12:05:24 +0200

    Add files via upload

---
+++ Scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject Pausemenu, PauseButton;
+
+	public void PauseHard()
+    {
+        Pausemenu.SetActive(true);
+        
+        Time.timeScale = 0;
+    }
+	
+	
+	public void Resume()
+    {
+        Pausemenu.SetActive(false);
+        
+        Time.timeScale = 1;
+    }
+}
