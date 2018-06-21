8fcf2edb0278ec3556577b5985b92e10a9d9d177
ClamDaButther <37106669+ClamDaButther@users.noreply.github.com>
2018-06-21 09:07:17 +0200

    Add files via upload

---
+++ Scripts/Fliping.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fliping : MonoBehaviour {
+
+    private bool facingRight;
+    private SpriteRenderer Charac;
+
+    private void Awake()
+    {
+        Charac = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        Flip(horizontal);
+    }
+
+    private void Flip(float horizontal)
+    {
+
+        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
+        {
+            /** facingRight = !facingRight;
+
+             Vector3 theScale = transform.localScale;
+
+             theScale.x *= -1;
+
+             transform.localScale = theScale;
+             **/
+
+            Charac.flipX = false;
+
+        }
+
+        else if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            Charac.flipX = true;
+        }
+
+    }
+}
---
+++ Scripts/Move.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour {
+
+     private Rigidbody2D myRigidbody;
+     private bool facingRight;
+     private bool isGrounded;
+     private bool jump;
+
+    [SerializeField]
+     private LayerMask whatIsGround;
+
+    [SerializeField]
+     private float jumpForce;
+
+    [SerializeField]
+     private float movementSpeed;
+
+    [SerializeField]
+     private Transform[] groundPoints;
+
+    [SerializeField]
+     private float groundRadius;
+
+void Start()
+  {
+    
+    myRigidbody = GetComponent<Rigidbody2D>();
+
+  }
+
+
+void FixedUpdate()
+  {
+    
+    float horizontal = Input.GetAxis("Horizontal");
+        isGrounded = IsGrounded();
+    HandleMovement(horizontal);
+
+        HandleInput();
+  }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump = true;
+            Debug.Log("111");
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] coliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < coliders.Length; i++)
+                {
+                    if (coliders[i].gameObject != gameObject)
+                    {
+
+                        return true;
+
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+        return false;
+
+
+    }
+
+    private void HandleMovement(float horizontal)
+  {
+
+    myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+
+        if (isGrounded == true && jump == true)
+        {
+            
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+            jump = false;
+            Debug.Log("111");
+        }
+  }
+
+}
