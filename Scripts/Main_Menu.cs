6eba191b91dfa72c6f01ed616dd53e614b5c93dd
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-07-06 14:40:02 +0200

    Update Main_Menu.cs

--- Scripts/Main_Menu.cs
+++ Scripts/Main_Menu.cs
@@ -9,18 +9,21 @@ public class Main_Menu : MonoBehaviour
     string lv1 = "Space Roamer";
     string lv2 = "Level2";
 
+    //Transition naar het main menu.
     public void LoadMenu()
     {
         SceneManager.LoadScene(Menu);
         Debug.Log("Aighty tighty.");
     }
 
+    //transitie naar het eerste level.
     public void LoadLvl1()
     {
         SceneManager.LoadScene(lv1);
         Debug.Log("YEEET");
     }
 
+    //Closes the game.
     public void Close()
     {
         Application.Quit();
